b6b61b5b434d71b47776d7a84fb8839a3a86c3e8
maxair2006 <sohamchindarkar58@gmail.com>
2021-03-25 16:49:19 +0530

    birdtrejectory

--- Bird.js
+++ Bird.js
@@ -2,11 +2,21 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.image2 = loadImage("sprites/smoke.png");
+    this.trejectory = [];
   }
 
   display() {
     //this.body.position.x = mouseX;
     //this.body.position.y = mouseY;
     super.display();
+    var p = this.body.position;
+    if(this.body.velocity.x>10 && p.x>200){
+      var pos = [p.x,p.y];
+      this.trejectory.push(pos);
+    }
+    for(var i=0;i<this.trejectory.length;i++){
+      image(this.image2,this.trejectory[i][0],this.trejectory[i][1]);
+    }
   }
 }
